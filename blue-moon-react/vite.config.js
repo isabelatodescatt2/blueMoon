8e9436684c6683bef47435c7b71ba6e311584224
Isabela Todescatt <87498826+isabelatodescatt2@users.noreply.github.com>
2025-11-28 07:42:55 -0300

    Update vite.config.js

--- blue-moon-react/vite.config.js
+++ blue-moon-react/vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/blueMoon/',
   build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-      },
-    },
-  },
+    outDir: 'dist'
+  }
 })
